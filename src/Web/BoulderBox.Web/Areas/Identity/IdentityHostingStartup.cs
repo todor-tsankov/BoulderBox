cbb833e124c7ed2042988f7a50669f81f8c1c343
Todor Tsankov <portotodor@abv.bg>
2020-11-03 13:18:00 +0200

    Change login and register views.
    Scaffold the default asp.net core login and register views and adapt them to the bootstrap theme

---
+++ src/Web/BoulderBox.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BoulderBox.Data;
+using BoulderBox.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BoulderBox.Web.Areas.Identity.IdentityHostingStartup))]
+namespace BoulderBox.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
