06ab382656c21e75d879250c9042631a4a1d486c
Todor Tsankov <portotodor@abv.bg>
2020-12-08 11:22:27 +0200

    Add Sorting on Gyms index page.

--- src/Web/BoulderBox.Web/Areas/Places/Controllers/GymsController.cs
+++ src/Web/BoulderBox.Web/Areas/Places/Controllers/GymsController.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
-
+using BoulderBox.Data.Models;
 using BoulderBox.Services.Data.Places;
 using BoulderBox.Web.Controllers;
 using BoulderBox.Web.ViewModels.Common;
@@ -29,21 +31,33 @@ namespace BoulderBox.Web.Areas.Places.Controllers
             this.countriesService = countriesService;
         }
 
-        public IActionResult Index(int pageId = 1)
+        public IActionResult Index(SortingInputModel sorting, int pageId = 1)
         {
+            if (sorting.OrderBy == null)
+            {
+                sorting = new SortingInputModel()
+                {
+                    Ascending = true,
+                    OrderBy = "Name",
+                };
+            }
+
+            var orderBySelector = GetOrderBySelector(sorting);
             var skip = DefaultItemsPerPage * (pageId - 1);
 
             var gymsViewModel = new GymsViewModel()
             {
                 Gyms = this.gymsService
                     .GetMany<GymViewModel>(
-                        orderBySelector: x => x.Name,
+                        orderBySelector: orderBySelector,
+                        asc: sorting.Ascending,
                         skip: skip,
                         take: DefaultItemsPerPage),
 
                 Common = new CommonViewModel()
                 {
                     Pagination = this.GetPaginationModel(pageId, this.gymsService.Count()),
+                    Sorting = sorting,
                 },
             };
 
@@ -100,6 +114,20 @@ namespace BoulderBox.Web.Areas.Places.Controllers
             await this.gymsService.DeleteAsync(x => x.Id == id);
 
             return this.RedirectToAction("Index");
+        }
+
+        private static Expression<Func<Gym, object>> GetOrderBySelector(SortingInputModel sortingModel)
+        {
+            Expression<Func<Gym, object>> orderBySelect;
+
+            orderBySelect = sortingModel.OrderBy switch
+            {
+                "Name" => x => x.Name,
+                "BouldersCount" => x => x.Boulders.Count,
+                _ => x => x.Name,
+            };
+
+            return orderBySelect;
         }
     }
 }
