a4ca614a0a2d1bf4347fb71186a7241c90fdbaa1
Todor Tsankov <portotodor@abv.bg>
2020-12-08 15:08:41 +0200

    Add Privacy Page

--- src/Web/BoulderBox.Web/Controllers/HomeController.cs
+++ src/Web/BoulderBox.Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace BoulderBox.Web.Controllers
             return this.View();
         }
 
+        public IActionResult About()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
