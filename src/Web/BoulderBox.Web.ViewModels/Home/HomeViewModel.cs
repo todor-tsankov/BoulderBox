dd11c861543ca334e30fe0a118f4a0795b722006
Todor Tsankov <portotodor@abv.bg>
2020-12-18 19:14:55 +0200

    Add HomePage with its pictures and view models

---
+++ src/Web/BoulderBox.Web.ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using BoulderBox.Web.ViewModels.Boulders.Grades;
+using BoulderBox.Web.ViewModels.Boulders.Styles;
+
+namespace BoulderBox.Web.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<GradeViewModel> Grades { get; set; }
+
+        public IEnumerable<StyleViewModel> Styles { get; set; }
+    }
+}
--- src/Web/BoulderBox.Web/Controllers/HomeController.cs
+++ src/Web/BoulderBox.Web/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
 ﻿using System.Diagnostics;
-
+using BoulderBox.Services.Data.Boulders;
 using BoulderBox.Web.ViewModels;
-
+using BoulderBox.Web.ViewModels.Boulders.Grades;
+using BoulderBox.Web.ViewModels.Boulders.Styles;
+using BoulderBox.Web.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoulderBox.Web.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly IGradesService gradesService;
+        private readonly IStylesService stylesService;
+
+        public HomeController(IGradesService gradesService, IStylesService stylesService)
+        {
+            this.gradesService = gradesService;
+            this.stylesService = stylesService;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var homeViewModel = new HomeViewModel()
+            {
+                Grades = this.gradesService
+                    .GetMany<GradeViewModel>(orderBySelector: x => x.Text, asc: false),
+                Styles = this.stylesService
+                    .GetMany<StyleViewModel>(),
+            };
+
+            return this.View(homeViewModel);
         }
 
         public IActionResult Privacy()
